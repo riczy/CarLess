a1b9982060ed045172fbbd6e558a251af4395b6e
Yvette White <ycwhite@gmail.com>
2015-06-25 10:32:12 -0400

    Initial Commit

---
+++ CarLess/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CarLess
+//
+//  Created by Evie Hahaha on 6/25/15.
+//  Copyright (c) 2015 Galloway Mobile. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
