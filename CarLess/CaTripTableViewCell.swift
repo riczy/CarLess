a76ae6986d903cb0296ce745df5cf16ca5c3827b
Yvette White <ycwhite@gmail.com>
2015-09-25 17:08:22 -0400

    Consolidated source files. Specifically, moved CaTripTableViewCell into the CaTripsListController since that is the only place it will be referenced.

--- CarLess/CaTripTableViewCell.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class CaTripTableViewCell: UITableViewCell {
-
-    @IBOutlet weak var modeImageView: UIImageView!
-    @IBOutlet weak var startTimeLabel: UILabel!
-    @IBOutlet weak var distanceLabel: UILabel!
-    @IBOutlet weak var distanceUnitLabel: UILabel!
-}
--- CarLess/CaTripsListController.swift
+++ CarLess/CaTripsListController.swift
@@ -1,5 +1,13 @@
 import UIKit
 
+class CaTripTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var modeImageView: UIImageView!
+    @IBOutlet weak var startTimeLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var distanceUnitLabel: UILabel!
+}
+
 class CaTripsListController: UITableViewController {
     
     private static var CellReuseIdentifier = "TripTableViewCell"
