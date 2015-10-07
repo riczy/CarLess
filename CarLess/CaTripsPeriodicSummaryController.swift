bec3c10a9e89f1cac0c957673db5294194de5ee6
Yvette White <ycwhite@gmail.com>
2015-10-07 09:58:22 -0400

    Initiated the trips summary listing scene for issue #47.

---
+++ CarLess/CaTripsPeriodicSummaryController.swift
@@ -0,0 +1,109 @@
+import UIKit
+
+class CaTripsPeriodicSummaryCell: UITableViewCell {
+   
+    var dateLabel: UILabel!
+    var tripsCountLabel: UILabel!
+    var moneySavedTotalLabel: UILabel!
+    var fuelSavedTotalLabel: UILabel!
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        dateLabel = UILabel()
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(dateLabel)
+        
+        tripsCountLabel = UILabel()
+        tripsCountLabel.translatesAutoresizingMaskIntoConstraints = false
+        tripsCountLabel.textAlignment = NSTextAlignment.Right
+        contentView.addSubview(tripsCountLabel)
+        
+        fuelSavedTotalLabel = UILabel()
+        fuelSavedTotalLabel.translatesAutoresizingMaskIntoConstraints = false
+        fuelSavedTotalLabel.textAlignment = NSTextAlignment.Right
+        contentView.addSubview(fuelSavedTotalLabel)
+        
+        moneySavedTotalLabel = UILabel()
+        moneySavedTotalLabel.translatesAutoresizingMaskIntoConstraints = false
+        moneySavedTotalLabel.textAlignment = NSTextAlignment.Right
+        contentView.addSubview(moneySavedTotalLabel)
+        
+        let size = contentView.frame.size
+        let width = size.width / 4
+        
+        contentView.addConstraint(NSLayoutConstraint(item: dateLabel, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem:  contentView, attribute: NSLayoutAttribute.LeftMargin, multiplier: 1.0, constant: 0.0))
+        contentView.addConstraint(NSLayoutConstraint(item: dateLabel, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: contentView, attribute: NSLayoutAttribute.CenterYWithinMargins, multiplier: 1.0, constant: 0.0))
+        contentView.addConstraint(NSLayoutConstraint(item: dateLabel, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: width))
+
+        contentView.addConstraint(NSLayoutConstraint(item: tripsCountLabel, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: dateLabel, attribute: NSLayoutAttribute.Right, multiplier: 1.0, constant: 0.0))
+        contentView.addConstraint(NSLayoutConstraint(item: tripsCountLabel, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: contentView, attribute: NSLayoutAttribute.CenterYWithinMargins, multiplier: 1.0, constant: 0.0))
+        contentView.addConstraint(NSLayoutConstraint(item: tripsCountLabel, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: width))
+        
+        contentView.addConstraint(NSLayoutConstraint(item: fuelSavedTotalLabel, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: tripsCountLabel, attribute: NSLayoutAttribute.Right, multiplier: 1.0, constant: 0.0))
+        contentView.addConstraint(NSLayoutConstraint(item: fuelSavedTotalLabel, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: contentView, attribute: NSLayoutAttribute.CenterYWithinMargins, multiplier: 1.0, constant: 0.0))
+        contentView.addConstraint(NSLayoutConstraint(item: fuelSavedTotalLabel, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: width))
+        
+        contentView.addConstraint(NSLayoutConstraint(item: moneySavedTotalLabel, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: fuelSavedTotalLabel, attribute: NSLayoutAttribute.Right, multiplier: 1.0, constant: 0.0))
+        contentView.addConstraint(NSLayoutConstraint(item: moneySavedTotalLabel, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: contentView, attribute: NSLayoutAttribute.CenterYWithinMargins, multiplier: 1.0, constant: 0.0))
+        contentView.addConstraint(NSLayoutConstraint(item: moneySavedTotalLabel, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: width))
+        
+    }
+}
+
+
+class CaTripsPeriodicSummaryController: UITableViewController {
+    
+    var data: [CaTripsSummary]!
+
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+        let trips = CaDataManager.instance.fetchTrips()
+        var collator = CaTripsSummaryCollator(data: trips, period: .Weekly)
+        data = collator.collate()
+        tableView.registerClass(CaTripsPeriodicSummaryCell.self, forCellReuseIdentifier: "Cell")
+        tableView.reloadData()
+   }
+
+    // MARK: - Table View Data Source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return data.count + 1
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! CaTripsPeriodicSummaryCell
+        if indexPath.row == 0 {
+            
+            cell.dateLabel.text = "Week"
+            cell.tripsCountLabel.text = "Trips"
+            cell.moneySavedTotalLabel.text = "$"
+            cell.fuelSavedTotalLabel.text = "Gallons"
+        } else {
+            
+            let summary = data[indexPath.row - 1]
+            cell.dateLabel.text = CaFormatter.date.stringFromDate(summary.startDate)
+            cell.tripsCountLabel.text = "\(summary.tripsCount)"
+            cell.moneySavedTotalLabel.text = CaFormatter.money.stringFromNumber(summary.moneySavedTotal)
+            cell.fuelSavedTotalLabel.text = CaFormatter.distance.stringFromNumber(summary.fuelSavedTotal)
+        }
+//        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+//        applyStyleForCell(cell)
+        
+        return cell
+    }
+    
+}
---
+++ CarLess/CaTripsSummary.swift
@@ -0,0 +1,91 @@
+import Foundation
+
+class CaTripsSummary: NSObject {
+    
+    var startDate: NSDate
+    var endDate: NSDate
+    var tripsCount: Int = 0
+    var moneySavedTotal: Double = 0.0
+    var fuelSavedTotal: Double = 0.0
+    
+    override var description: String {
+        get {
+            return "\(startDate) - \(endDate) | tripsCount = \(tripsCount), moneySavedTotal = \(moneySavedTotal), fuelSavedTotal = \(fuelSavedTotal)"
+        }
+    }
+    
+    convenience init(startDate: NSDate, endDate: NSDate) {
+        
+        self.init(startDate: startDate, endDate: endDate, tripsCount: 0, moneySavedTotal: 0.0, fuelSavedTotal: 0.0)
+    }
+    
+    init(startDate: NSDate, endDate: NSDate, tripsCount: Int, moneySavedTotal: Double, fuelSavedTotal: Double) {
+        
+        self.startDate = startDate
+        self.endDate = endDate
+        self.tripsCount = tripsCount
+        self.moneySavedTotal = moneySavedTotal
+        self.fuelSavedTotal = fuelSavedTotal
+    }
+    
+    func isWithinRange(trip: Trip) -> Bool {
+    
+        return trip.startTimestamp.isOnOrAfter(startDate) && trip.startTimestamp.isOnOrBefore(endDate)
+    }
+    
+    func add(trip: Trip) -> Bool {
+        
+        if isWithinRange(trip) {
+            
+            ++tripsCount
+            if let money = trip.moneySaved() {
+                moneySavedTotal += money
+            }
+            if let fuel = trip.fuelSaved() {
+                fuelSavedTotal += fuel
+            }
+            return true
+        }
+        return false
+    }
+    
+}
+
+struct CaTripsSummaryCollator {
+    
+    enum CollatorPeriod {
+        case Weekly
+        case Monthly
+    }
+
+    var data: [Trip]!
+    var period: CollatorPeriod!
+    private var summaryDataMap = [NSDate : CaTripsSummary]()
+    
+    init(data: [Trip], period: CollatorPeriod) {
+        
+        self.data = data
+        self.period = period
+    }
+    
+    mutating func collate() -> [CaTripsSummary] {
+        
+        for trip in data {
+            let dateRange: (startDate: NSDate, endDate: NSDate) = period == CollatorPeriod.Weekly ? trip.startTimestamp.weekRange() : trip.startTimestamp.monthRange()
+            if let value = summaryDataMap[dateRange.startDate] {
+                value.add(trip)
+            } else {
+                let summary = CaTripsSummary(startDate: dateRange.startDate, endDate: dateRange.endDate)
+                summary.add(trip)
+                summaryDataMap[dateRange.startDate] = summary
+            }
+        }
+        var results = [CaTripsSummary]()
+        let sortedMap = summaryDataMap.sort { $0.0.isAfter($1.0) }
+        for (_, value) in sortedMap {
+            results.append(value)
+        }
+        return results
+    }
+    
+}
