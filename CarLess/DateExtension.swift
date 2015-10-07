fb256cc6a2242cc8f10d1f77103bd38e9ef32634
Yvette White <ycwhite@gmail.com>
2015-10-07 08:56:34 -0400

    Added a method that provide the beginning and end of the month in which the date resides. Added a method that does the same for a week range.

--- CarLess/DateExtension.swift
+++ CarLess/DateExtension.swift
@@ -24,4 +24,32 @@ extension NSDate {
         return result == .OrderedSame || result == .OrderedDescending
     }
     
+    public func monthRange() -> (startDate: NSDate, endDate: NSDate) {
+        
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components([NSCalendarUnit.Era, NSCalendarUnit.Year, NSCalendarUnit.Month], fromDate: self)
+        let startDate = calendar.dateFromComponents(components)
+        let monthDays = calendar.rangeOfUnit(NSCalendarUnit.Day, inUnit: NSCalendarUnit.Month, forDate: self)
+        
+        components.day = monthDays.length
+        components.hour = 23
+        components.minute = 59
+        components.second = 59
+        
+        let endDate = calendar.dateFromComponents(components)
+        
+        return (startDate!, endDate!)
+    }
+    
+    public func weekRange() -> (startDate: NSDate, endDate: NSDate) {
+        
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components([NSCalendarUnit.YearForWeekOfYear, NSCalendarUnit.WeekOfYear], fromDate: self)
+        let startDate = calendar.dateFromComponents(components)
+        var endDate = calendar.dateByAddingUnit(NSCalendarUnit.Day, value: 6, toDate: startDate!, options: NSCalendarOptions(rawValue: 0))
+        endDate = calendar.dateBySettingHour(23, minute: 59, second: 59, ofDate: endDate!, options: NSCalendarOptions(rawValue: 0))
+        
+        return (startDate!, endDate!)
+    }
+    
 }
